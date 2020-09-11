a54dbdbc504c84b3b8dc5607a88e92cc5b9540c5
Tia TeHiko <tiatehiko@gmail.com>
2020-09-11 22:01:28 +1200

    import images

--- server/db/seeds/candles.js
+++ server/db/seeds/candles.js
@@ -6,7 +6,7 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('candles').insert([
         {id: 1, name: 'Carousel Mini', price: 8, img: './images/1.jpg', details: 'Coming Soon...', inCart: false, count: 0, total: 0},
-        { id: 2,name: 'Carousel Large',price: 20,img: './images/2.jpg',details: 'Coming Soon...',inCart: false,count: 0,total: 0},
+        {id: 2,name: 'Carousel Large',price: 20,img: './images/2.jpg',details: 'Coming Soon...',inCart: false,count: 0,total: 0},
         {id: 3,name: 'Vintage Clear',price: 25,img: './images/1.jpg',details: 'Coming Soon...',inCart: false,count: 0,total: 0},                                                                                                                                                                            
         {id: 4,name: 'Vintage Tiffany Blue',price: 25,img: './images/4.jpg',details: 'Coming Soon...',inCart: false,count: 0,total: 0},
         {id: 5,name: 'Love Jar',price: 25,img: './images/7.jpg',details: 'Coming Soon...',inCart: false,count: 0,total: 0},
