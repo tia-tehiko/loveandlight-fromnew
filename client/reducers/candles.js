8a07130650cdd0b22bfd1e7f51410e10b3948bba
Tia TeHiko <tiatehiko@gmail.com>
2020-09-12 12:41:59 +1200

    create working action and test

--- client/reducers/candles.js
+++ client/reducers/candles.js
@@ -1,16 +1,12 @@
-const initialState = [
-    {id: 1, name: 'Carousel Mini', price: 8, img: './images/1.jpg', details: 'Coming Soon...', inCart: false, count: 0, total: 0},
-    {id: 2,name: 'Carousel Large',price: 20,img: './images/2.jpg',details: 'Coming Soon...',inCart: false,count: 0,total: 0},
-    {id: 3,name: 'Vintage Clear',price: 25,img: './images/1.jpg',details: 'Coming Soon...',inCart: false,count: 0,total: 0},                                                                                                                                                                            
-    {id: 4,name: 'Vintage Tiffany Blue',price: 25,img: './images/4.jpg',details: 'Coming Soon...',inCart: false,count: 0,total: 0},
-    {id: 5,name: 'Love Jar',price: 25,img: './images/7.jpg',details: 'Coming Soon...',inCart: false,count: 0,total: 0},
-    {id: 6,name: 'Geo Large',price: 35,img: './images/4.jpg',details: 'Coming Soon...',inCart: false,count: 0,total: 0},
-    {id: 7,name: 'Renee Jar',price: 35,img: './images/2.jpg',details: 'Coming Soon...',inCart: false,count: 0,total: 0},
-    {id: 8,name: 'Renee Mauve',price: 35,img: './images/5.jpg',details: 'Coming Soon...',inCart: false,count: 0,total: 0},
-    {id: 9,name: 'Oh Jar',price: 40,img: './images/2.jpg',details: 'Coming Soon...',inCart: false,count: 0,total: 0}
-  ]
+import { FETCH_CANDLES } from "../actions";
 
-export default function candlesReducer (state = initialState, action) {
-    return state
+export default function candlesReducer (state = [], action) {
+  switch(action.type) {
+    case FETCH_CANDLES:
+    return action.candles
+
+    default:
+      return state
+  }
 }
 
--- client/reducers/candles.test.js
+++ client/reducers/candles.test.js
@@ -4,3 +4,4 @@ test('initial state has 9 tasks', () => {
     let state = candlesReducer(undefined, {})
     expect(state.length).toBe(9)
 })
+
