e15d9d059e7f77e08f1a8eda05f1274e3a6386cd
Tia TeHiko <tiatehiko@gmail.com>
2020-09-12 13:33:02 +1200

    create diffuser action

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,8 +1,16 @@
 export const FETCH_CANDLES = 'FETCH_CANDLES'
+export const FETCH_DIFFUSERS = 'FETCH_DIFFUSERS'
 
 export function fetchCandles (candles) {
     return ({
         type: FETCH_CANDLES,
         candles
+    })
+}
+
+export function fetchDiffusers (diffusers) {
+    return ({
+        type: FETCH_DIFFUSERS,
+        diffusers
     })
 }
