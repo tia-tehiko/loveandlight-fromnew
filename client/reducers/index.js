36ca459f3633b33cc09e1a007ac637b2a5691542
Tia TeHiko <tiatehiko@gmail.com>
2020-09-12 13:33:17 +1200

    import diffuser reducer to combine reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux"
 
 import candlesReducer from './candles'
+import diffusersReducer from './diffusers'
 import cartReducer from "./cart"
 
 const reducers = combineReducers({
     candles: candlesReducer,
+    diffusers: diffusersReducer,
     cartItems: cartReducer
 })
 
