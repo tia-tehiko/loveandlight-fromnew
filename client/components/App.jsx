81f2e5edc9f58aa3e609e7a7991d7a972c2f90d5
Tia TeHiko <tiatehiko@gmail.com>
2020-09-12 13:00:30 +1200

    move candles into own component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,39 +1,18 @@
 import React from 'react'
 import Candles from './Candles'
 import { connect } from 'react-redux'
-import { getCandles } from "../api";
-import { fetchCandles } from "../actions";
-
-
 
 export class App extends React.Component {
-
-  componentDidMount() {
-    getCandles()
-    .then(candles => {
-      this.props.dispatch(fetchCandles(candles))
-    })
-    .catch(err => {
-      console.log(err)
-    })
-  }
   
   render() {
     return (
       <div>
         <h1>App Page</h1>
-        <ul>{this.props.candles.map(candle => 
-          <li key={candle.id}>{candle.name}</li>)}
-        </ul>
+        <Candles />
       </div>
     )
   }
 }
 
-function mapStateToProps(state) {
-  return {
-    candles: state.candles
-  }
-}
 
-export default connect(mapStateToProps)(App)
+export default App
--- client/components/Candles.jsx
+++ client/components/Candles.jsx
@@ -1,17 +1,35 @@
 import React from 'react'
-
+import { connect } from 'react-redux'
 import { getCandles } from "../api";
+import { fetchCandles } from "../actions";
 
+class Candles extends React.Component {
+    componentDidMount() {
+        getCandles()
+        .then(candles => {
+          this.props.dispatch(fetchCandles(candles))
+        })
+        .catch(err => {
+          console.log(err)
+        })
+      }
 
-export class Candles extends React.Component {
     render() {
         return (
             <div>
                 <h1>Vegan Soy Candles</h1>
-              
+                <ul>{this.props.candles.map(candle => 
+                    <li key={candle.id}>{candle.name}</li>)}
+                </ul>     
             </div>
         )
     }
 }
 
-export default Candles
+function mapStateToProps(state) {
+  return {
+    candles: state.candles
+  }
+}
+
+export default connect(mapStateToProps)(Candles)
