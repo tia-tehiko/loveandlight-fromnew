8cef3bc64e887f092a836b543027e0c4030ba2ac
Tia TeHiko <tiatehiko@gmail.com>
2020-09-13 16:57:18 +1200

    create db function and route for scents db

--- server/db/db.js
+++ server/db/db.js
@@ -2,15 +2,20 @@ const environment = process.env.NODE_ENV || 'development'
 const config = require('./knexfile')[environment]
 const connection = require('knex')(config)
 
-function getCandles (db = connection) {
-    return db('candles').select()
+function getCandles(db = connection) {
+  return db('candles').select()
 }
 
-function getDiffusers (db = connection) {
-    return db('diffusers').select()
+function getDiffusers(db = connection) {
+  return db('diffusers').select()
+}
+
+function getScents(db = connection) {
+  return db('scents').select()
 }
 
 module.exports = {
-    getCandles,
-    getDiffusers
+  getCandles,
+  getDiffusers,
+  getScents,
 }
---
+++ server/routes/routes.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router()
+
+const db = require('../db/db')
+
+router.get('/candles', (req, res) => {
+  db.getCandles().then((candles) => {
+    res.json(candles)
+  })
+})
+
+router.get('/diffusers', (req, res) => {
+  db.getDiffusers().then((diffusers) => {
+    res.json(diffusers)
+  })
+})
+
+router.get('/scents', (req, res) => {
+  db.getScents().then((scents) => {
+    res.json(scents)
+  })
+})
+
+module.exports = router
