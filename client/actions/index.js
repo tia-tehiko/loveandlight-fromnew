b8e7e4008e2372ebc1ba6b890de4aeaf1387314c
Tia TeHiko <tiatehiko@gmail.com>
2020-09-13 15:07:02 +1200

    add DISPLAY redeucer functions

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,16 +1,38 @@
 export const FETCH_CANDLES = 'FETCH_CANDLES'
 export const FETCH_DIFFUSERS = 'FETCH_DIFFUSERS'
+export const DISPLAY_CANDLE_DETAILS = 'DISPLAY_CANDLE_DETAILS'
+export const DISPLAY_DIFFUSER_DETAILS = 'DISPLAY_DIFFUSER_DETAILS'
 
-export function fetchCandles (candles) {
+export function fetchCandles(candles) {
     return ({
         type: FETCH_CANDLES,
         candles
     })
 }
 
-export function fetchDiffusers (diffusers) {
+export function fetchDiffusers(diffusers) {
     return ({
         type: FETCH_DIFFUSERS,
         diffusers
+    })
+}
+
+export function displayCandleDetails(id) {
+    return ({
+        type: DISPLAY_CANDLE_DETAILS,
+        id,
+        name,
+        img,
+        price
+    })
+}
+
+export function displayDiffuserDetails(id) {
+    return ({
+        type: DISPLAY_DIFFUSER_DETAILS,
+        id,
+        name,
+        img,
+        price
     })
 }
