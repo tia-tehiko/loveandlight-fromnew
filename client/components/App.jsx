35defe8e0ddfd007d359f9db347caa50e240fc3a
Tia TeHiko <tiatehiko@gmail.com>
2020-09-13 16:13:07 +1200

    add new details page routes to main app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -5,7 +5,9 @@ import { connect } from 'react-redux'
 import Nav from './Nav'
 import LandingPage from './LandingPage'
 import Candles from './Candles'
+import CandleDetails from './CandleDetails'
 import Diffusers from './Diffusers'
+import DiffuserDetails from './DiffuserDetails'
 
 export class App extends React.Component {
   render() {
@@ -14,7 +16,9 @@ export class App extends React.Component {
         <Route path='/' component={Nav} />
         <Route exact path='/' component={LandingPage} />
         <Route exact path='/candles' component={Candles} />
+        <Route exact path='/candles/:name' component={CandleDetails} />
         <Route exact path='/diffusers' component={Diffusers} />
+        <Route exact path='/diffusers/:name' component={DiffuserDetails} />
       </div>
     )
   }
