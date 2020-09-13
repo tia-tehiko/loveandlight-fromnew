070d7d3ceac56eeb63cad6c545c43cf5f7ae9b44
Tia TeHiko <tiatehiko@gmail.com>
2020-09-13 16:11:06 +1200

    create action to display individual product

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,38 +1,24 @@
 export const FETCH_CANDLES = 'FETCH_CANDLES'
 export const FETCH_DIFFUSERS = 'FETCH_DIFFUSERS'
-export const DISPLAY_CANDLE_DETAILS = 'DISPLAY_CANDLE_DETAILS'
-export const DISPLAY_DIFFUSER_DETAILS = 'DISPLAY_DIFFUSER_DETAILS'
+export const DISPLAY_PRODUCT_DETAILS = 'DISPLAY_PRODUCT_DETAILS'
 
 export function fetchCandles(candles) {
-    return ({
-        type: FETCH_CANDLES,
-        candles
-    })
+  return {
+    type: FETCH_CANDLES,
+    candles,
+  }
 }
 
 export function fetchDiffusers(diffusers) {
-    return ({
-        type: FETCH_DIFFUSERS,
-        diffusers
-    })
+  return {
+    type: FETCH_DIFFUSERS,
+    diffusers,
+  }
 }
 
-export function displayCandleDetails(id) {
-    return ({
-        type: DISPLAY_CANDLE_DETAILS,
-        id,
-        name,
-        img,
-        price
-    })
-}
-
-export function displayDiffuserDetails(id) {
-    return ({
-        type: DISPLAY_DIFFUSER_DETAILS,
-        id,
-        name,
-        img,
-        price
-    })
+export function displayProductDetails(id) {
+  return {
+    type: DISPLAY_PRODUCT_DETAILS,
+    id,
+  }
 }
