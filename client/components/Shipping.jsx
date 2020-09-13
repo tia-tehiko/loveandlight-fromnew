5882e04a2fde105d78f2bb8a18ae8c0f8b520f7e
Tia TeHiko <tiatehiko@gmail.com>
2020-09-13 16:39:09 +1200

    create shipping and returns page

---
+++ client/components/Shipping.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+export class Shipping extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Shipping & Returns</h1>
+        <h3>Shipping</h3>
+        <p>
+          Once your order has been placed we will start to prepare your order.
+          Every diffuser and candle is hand poured with love by myself with
+          making sure that each fragrance is set to perfection and ensuring your
+          home fragrances smell fresh and amazing.
+        </p>
+        <p>
+          We aim to ship your order to you within 12-48 hours from completion of
+          your order
+        </p>
+        <p>
+          Brisbane Metro Area - For orders up to 5kg we charge a Flat Rate of $9
+        </p>
+        <p>
+          Australia Wide - For orders up to 5kg we charge a Flat Rate of $14
+        </p>
+        <p>
+          We offer free local pick up from Wakerley (Brisbane) or alternatively
+          at one of our market days on a Sunday (Redcliffe, Manly). You will be
+          notified via email when your order is ready for pick up
+        </p>
+        <p>
+          We send all orders via Wiz Me courier service (Couriers Please).
+          Regular shipping arrives within 2-4 business days for the Metropolitan
+          Areas from when your order has been dispatched. For regional areas,
+          your order may take 4-10 business days to get to you. If you have not
+          received your order by 10 working days from dispatch please contact us
+          at loveandlighthomefragrances@gmail.com (Please note: Times may vary
+          depending on public holidays, Christmas holiday periods
+        </p>
+        <p>Please note we do not deliver to parcel lockers or PO boxes</p>
+
+        <h3>Returns</h3>
+        <p>
+          Product information is clearly described in each listing, we do not
+          offer a refund due to change of mind
+        </p>
+        <p>
+          We pride ourselves on delivering you with the best quality products as
+          time and care is taken with producing your products and packaging them
+          ready for postage. Each item in your order is thoroughly inspected
+          before leaving our studio and each parcel is clearly marked with
+          "Fragile" before shipping. If for some reason you do receive a faulty
+          item please contact us within 48hrs of receiving your order by
+          emailing loveandlighthomefragrances@gmail.com with your name, address,
+          order number and a photo of your faulty item
+        </p>
+      </div>
+    )
+  }
+}
+
+export default Shipping
