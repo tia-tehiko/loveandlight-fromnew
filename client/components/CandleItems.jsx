d5bce84150bcfd4c33e2aaffdf0cfb9e2bf06004
Tia TeHiko <tiatehiko@gmail.com>
2020-09-13 16:12:25 +1200

    add handleClick function to display correct product details

--- client/components/CandleItems.jsx
+++ client/components/CandleItems.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { displayProductDetails } from '../actions'
 
 class CandleItems extends React.Component {
+  handleClick = (id) => {
+    this.props.dispatch(displayProductDetails(id))
+  }
   render() {
     const { id, name, img, price } = this.props.candle
     return (
       <div className='product'>
-        <Link key={id} to={`candles/${name}`}>
-          <li>
-            <img src={img} className='productImage'></img>
-            <h4 className='productHeader'> {name}</h4>
-            <h5 className='productPrice'> ${price}</h5>
-          </li>
+        <Link
+          key={id}
+          to={`candles/${name}`}
+          onClick={() => this.handleClick(id)}
+        >
+          <img src={img} className='productImage'></img>
+          <h4 className='productHeader'> {name}</h4>
+          <h5 className='productPrice'> ${price}</h5>
         </Link>
       </div>
     )
