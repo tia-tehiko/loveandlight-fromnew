c65d9c007d49ba2af904a4f48444bad93006a9c3
Tia TeHiko <tiatehiko@gmail.com>
2020-09-13 16:11:30 +1200

    add display product action to reducer

--- client/reducers/candles.js
+++ client/reducers/candles.js
@@ -1,17 +1,14 @@
-import { FETCH_CANDLES, DISPLAY_CANDLE_DETAILS } from "../actions";
+import { FETCH_CANDLES, DISPLAY_PRODUCT_DETAILS } from '../actions'
 
 export default function candlesReducer(state = [], action) {
   switch (action.type) {
     case FETCH_CANDLES:
       return action.candles
 
-    case DISPLAY_CANDLE_DETAILS:
-      return state.filter(item => item.id !== action.id)
+    case DISPLAY_PRODUCT_DETAILS:
+      return state.filter((item) => item.id === action.id)
 
     default:
       return state
   }
 }
-
-
-
--- client/reducers/diffusers.js
+++ client/reducers/diffusers.js
@@ -1,14 +1,14 @@
-import { FETCH_DIFFUSERS, DISPLAY_DIFFUSER_DETAILS } from "../actions"
+import { FETCH_DIFFUSERS, DISPLAY_PRODUCT_DETAILS } from '../actions'
 
 export default function diffusersReducer(state = [], action) {
-    switch (action.type) {
-        case FETCH_DIFFUSERS:
-            return action.diffusers
+  switch (action.type) {
+    case FETCH_DIFFUSERS:
+      return action.diffusers
 
-        case DISPLAY_DIFFUSER_DETAILS:
-            return state.filter(item => item.id !== action.id)
+    case DISPLAY_PRODUCT_DETAILS:
+      return state.filter((item) => item.id === action.id)
 
-        default:
-            return state
-    }
-} 
+    default:
+      return state
+  }
+}
