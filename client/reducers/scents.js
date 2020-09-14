a9f36b10cc7a905cceb45ee548caf3877ca25673
Tia TeHiko <tiatehiko@gmail.com>
2020-09-15 11:58:34 +1200

    remove select_scent action

--- client/reducers/scents.js
+++ client/reducers/scents.js
@@ -1,13 +1,9 @@
-import { FETCH_SCENTS, SELECT_SCENT } from '../actions'
+import { FETCH_SCENTS } from '../actions'
 
 export default function scentsReducer(state = [], action) {
   switch (action.type) {
     case FETCH_SCENTS:
       return action.scents
-
-    case SELECT_SCENT:
-      return state.filter((scent) => scent.id === action.id)
-
     default:
       return state
   }
