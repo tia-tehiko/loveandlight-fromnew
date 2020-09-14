e9d8cfbd23fec58dbe55397429c82ad01cfd1f03
Tia TeHiko <tiatehiko@gmail.com>
2020-09-14 18:31:44 +1200

    create select scenttype to reducer

--- client/reducers/scents.js
+++ client/reducers/scents.js
@@ -1,9 +1,12 @@
-import { FETCH_SCENTS } from '../actions'
+import { FETCH_SCENTS, SELECT_SCENT } from '../actions'
 
 export default function scentsReducer(state = [], action) {
   switch (action.type) {
     case FETCH_SCENTS:
       return action.scents
+
+    case SELECT_SCENT:
+      return state.filter((scent) => scent.id === action.id)
 
     default:
       return state
