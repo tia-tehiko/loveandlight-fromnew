cfcd3afde655a64923ba4f19041f0e1f73789e44
Tia TeHiko <tiatehiko@gmail.com>
2020-09-14 18:33:11 +1200

    update addtocart reducer

--- client/reducers/cart.js
+++ client/reducers/cart.js
@@ -1,10 +1,13 @@
+import { ADD_TO_CART } from '../actions'
+
 const initialState = []
 
 export default function cartReducer(state = initialState, action) {
   switch (action.type) {
-    case 'ADD_TO_CART':
-      return { ...state, cartItems: state.map(item => item.id === action.id ? { ...item, inCart: true } : item) }
-
+    case ADD_TO_CART:
+      return [...state, {
+        id: action.id,
+      }]
     case 'REMOVE_FROM_CART':
       return state.filter(item => item.id !== action.id)
     case 'UPDATE_CART':
@@ -13,3 +16,4 @@ export default function cartReducer(state = initialState, action) {
       return state
   }
 }
+
