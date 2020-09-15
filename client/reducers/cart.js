7a8a8a1918d404e6dc03a5e644d8400a50efccdf
Tia TeHiko <tiatehiko@gmail.com>
2020-09-15 16:35:07 +1200

    update cartReducer

--- client/reducers/cart.js
+++ client/reducers/cart.js
@@ -6,14 +6,11 @@ export default function cartReducer(state = initialState, action) {
   switch (action.type) {
     case ADD_TO_CART:
       return [...state, {
-        id: action.id,
+        ...action.item,
+        scent: state.scentId,
+        quantity: 1,
       }]
-    case 'REMOVE_FROM_CART':
-      return state.filter(item => item.id !== action.id)
-    case 'UPDATE_CART':
-      return action.cart
     default:
       return state
   }
 }
-
