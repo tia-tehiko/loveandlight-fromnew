d2ebbc747f6b2e4f666153a173b4ed1c738f30e1
Tia TeHiko <tiatehiko@gmail.com>
2020-09-15 12:27:15 +1200

    seed new data

--- server/db/migrations/20200907153414_candles.js
+++ server/db/migrations/20200907153414_candles.js
@@ -1,19 +1,21 @@
 
-exports.up = function(knex) {
-    return knex.schema.createTable('candles', (table) => {
-        table.increments('id').primary()
-        table.string('name')
-        table.integer('price')
-        table.string('img')
-        table.string('details')
-        table.boolean('inCart')
-        table.integer('count')
-        table.integer('total')
-    })
-  };
-  
-  exports.down = function(knex) {
-    return knex.schema.dropTable('candles')
-  };
-  
-  
+exports.up = function (knex) {
+  return knex.schema.createTable('candles', (table) => {
+    table.increments('id').primary()
+    table.string('name')
+    table.string('size')
+    table.string('wick')
+    table.integer('price')
+    table.string('img')
+    table.string('details')
+    table.string('gift_boxed')
+    table.boolean('inCart')
+    table.integer('count')
+    table.integer('total')
+  })
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('candles')
+};
+
