613599c50ec131e28cc49a28882be5e3dc95fc61
Tia TeHiko <tiatehiko@gmail.com>
2020-09-22 11:55:23 +1200

    create post db funcction

--- server/db/db.js
+++ server/db/db.js
@@ -14,8 +14,19 @@ function getScents(db = connection) {
   return db('scents').select()
 }
 
+function addToCart(item, scent, db = connection) {
+  const { name, img, price } = item
+  return db('cart').insert({
+    name: name,
+    img: img,
+    price: price,
+    scent: scent,
+  })
+}
+
 module.exports = {
   getCandles,
   getDiffusers,
   getScents,
+  addToCart,
 }
