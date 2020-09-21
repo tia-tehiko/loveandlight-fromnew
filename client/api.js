9639f52fbab6b6a7778bf5b93b706130fe3ba0c8
Tia TeHiko <tiatehiko@gmail.com>
2020-09-22 11:46:21 +1200

    create postToCart api

--- client/api.js
+++ client/api.js
@@ -3,21 +3,24 @@ import request from 'superagent'
 export function getCandles() {
   return request
     .get('http://localhost:3000/api/v1/candles')
-    .then(response => response.body)
+    .then((response) => response.body)
 }
-
 
 export function getDiffusers() {
   return request
     .get('http://localhost:3000/api/v1/diffusers')
-    .then(response => response.body)
+    .then((response) => response.body)
 }
 
 export function getScents() {
   return request
     .get('http://localhost:3000/api/v1/scents')
-    .then(response => response.body)
+    .then((response) => response.body)
 }
 
-
-
+export function postToCart(data) {
+  return request
+    .post('/api/v1/cart')
+    .send({ item: data })
+    .then((response) => response.body)
+}
