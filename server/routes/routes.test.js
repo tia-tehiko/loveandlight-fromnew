5be1e2fbda050225dfe30b32ed1af2f75b275779
Tia TeHiko <tiatehiko@gmail.com>
2020-10-22 11:21:34 +1000

    test getDiffuser routes

--- server/routes/routes.test.js
+++ server/routes/routes.test.js
@@ -19,7 +19,7 @@ describe('GET/  api/v1/candles', () => {
       })
   })
 
-  test('candles are returned correctly', () => {
+  test(' check if candles are returned correctly', () => {
     getCandles.mockImplementation(() => Promise.resolve([
       {
         id: 1,
@@ -39,9 +39,49 @@ describe('GET/  api/v1/candles', () => {
       .then((res) => {
         expect.assertions(2)
         expect(res.body[0].name).toBe('Carousel Mini')
-        expect(res.body[1].name).toBe('Carousel Large')
+        expect(res.body[1].price).toBe(25)
       })
   })
 })
 
+describe('GET/ api/v1/diffusers', () => {
+  test('check if status 200', () => {
+    getDiffusers.mockImplementation(() => Promise.resolve())
+    return request(server)
+      .get('/api/v1/diffusers')
+      .then((res) => {
+        expect.assertions(1)
+        expect(res.status).toBe(200)
+      })
+  })
+
+  test('check if diffusers are returned correctly', () => {
+    getDiffusers.mockImplementation(() => Promise.resolve([
+      {
+        id: 1,
+        name: 'Reed Diffuser',
+        price: 25,
+      },
+      {
+        id: 2,
+        name: 'Round Car Diffuser',
+        price: 10,
+      },
+      {
+        id: 3,
+        name: 'Circle Car Diffuser',
+        price: 10,
+      }
+    ]))
+    return request(server)
+      .get('/api/v1/diffusers')
+      .then((res) => {
+        expect.assertions(3)
+        expect(res.body[0].name).toBe('Reed Diffuser')
+        expect(res.body[1].price).toBe(10)
+        expect(res.body[2].id).toBe(3)
+      })
+
+  })
+})
 
