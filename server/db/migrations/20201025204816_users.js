6fec02b15c5398acb8e0377c1a0ec7ebb03b8baa
Josiah Waikari <teariki.waikari@gmail.com>
2020-10-25 23:18:22 +1300

    [database] add user migrations

---
+++ server/db/migrations/20201025204816_users.js
@@ -0,0 +1,13 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('users', (table) => {
+    table.string('id').primary()
+    table.string('email')
+    table.string('hash')
+    table.string('salt')
+    table.boolean('admin')
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('users')
+}
