7dd359f1d66a7935c759c7c08c2753e473194cc3
Josiah Waikari <teariki.waikari@gmail.com>
2020-10-26 19:44:05 +1300

    [routes] make sure a user cant sign up with the same email

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const passport = require('passport')
-const { newUser } = require('../db/user.db')
+const { newUser, findOne } = require('../db/user.db')
 const guid = require('../utils/guid.util')
 const { genPassword } = require('../utils/password.util')
 const { isAuth } = require('./middleware/auth.middleware')
@@ -9,20 +9,39 @@ const router = express.Router()
 router.post('/login', passport.authenticate('local', { failureRedirect: 'login/failure', successRedirect: 'login/success' }))
 
 router.post('/register', async (req, res) => {
-  const saltHash = genPassword(req.body.password)
-    
-  const salt = saltHash.salt
-  const hash = saltHash.hash
+  const user = await findOne({ email: req.body.email })
 
-  const user = await newUser({
-    id: guid({ prefix: 'USR'}),
-    email: req.body.email,
-    hash: hash,
-    salt: salt,
-    admin: false
-  })
+  if (!user) {
+    const saltHash = genPassword(req.body.password)
 
-  res.json(user)
+    const salt = saltHash.salt
+    const hash = saltHash.hash
+
+    const user = await newUser({
+      id: guid({ prefix: 'USR'}),
+      name: req.body.name,
+      email: req.body.email,
+      hash: hash,
+      salt: salt,
+      admin: false
+    })
+
+    res.json({
+      success: true,
+      message: "user has successfully been registered",
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email
+      },
+      cookies: req.cookies
+    })
+  } else {
+    res.status(401).json({
+      success: false,
+      message: "user with this email already exists"
+    })
+  }
 })
 
 router.get('/login/success', isAuth, (req, res) => res.json({
