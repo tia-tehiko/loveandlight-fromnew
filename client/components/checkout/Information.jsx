4a840f1c89eff819c099619db13a734063e91b1c
Josiah Waikari <teariki.waikari@gmail.com>
2020-10-26 19:45:38 +1300

    [components] header and information component with breadcrumb of process and login/register form

---
+++ client/components/checkout/Information.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Form from '../user/Form'
+
+class Information extends React.Component {
+
+  render() {
+    return <Form />
+  }
+}
+
+export default Information
